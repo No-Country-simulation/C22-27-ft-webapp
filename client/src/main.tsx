e5578989c8f33de6989fd6bc8fa97e6eecbb9792
Daniel <daniel_alexis_12@hotmail.com>
2024-11-20 20:55:40 -0300

    optimiza sidebar y creacion del navbar

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes/route";
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 import './index.css'
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
