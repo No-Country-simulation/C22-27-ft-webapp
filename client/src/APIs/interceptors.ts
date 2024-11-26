0acb1049c49ce5d7945eaad2b94310fe27b540c5
santiagoweb2024 <santiagousca716@gmail.com>
2024-11-26 09:53:38 -0500

    feat: configuración inicial de Axios y tipos de ambiente
    - Agregar configuración base de Axios con interceptores
    - Crear archivo de tipos para variables de ambiente
    - Eliminar archivo borrar.txt no utilizado
    - Actualizar tsconfig para incluir tipos de ambiente

---
+++ client/src/APIs/interceptors.ts
@@ -0,0 +1,34 @@
+import { api } from "./configAxios";
+import { AxiosError, InternalAxiosRequestConfig, AxiosResponse } from "axios";
+
+const requestInterceptor = (config: InternalAxiosRequestConfig) => {
+    const authStorage = localStorage.getItem("auth-storage");
+    if (authStorage) {
+      const state = JSON.parse(authStorage).state;
+      const token = state.token;
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  };
+
+const requestErrorInterceptor = (error: AxiosError) => {
+  return Promise.reject(error);
+}
+
+const responseInterceptor = (response: AxiosResponse) => {
+  return response.data;
+}
+
+const responseErrorInterceptor = (error: AxiosError) => {
+  if (error.response?.status === 401) {
+    localStorage.removeItem("token");
+    window.location.href = "/login";
+  }
+  return Promise.reject(error);
+}
+
+api.interceptors.request.use(requestInterceptor, requestErrorInterceptor);
+api.interceptors.response.use(responseInterceptor, responseErrorInterceptor);
+
+export default api;
+
