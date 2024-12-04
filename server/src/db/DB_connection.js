9304a4d6ccb5ea37e6c85b7f8308ca4da66fee32
Milo <zapatacamilo86@gmail.com>
2024-12-04 13:41:50 -0500

    DB_connection.js update

--- server/src/db/DB_connection.js
+++ server/src/db/DB_connection.js
@@ -5,7 +5,7 @@ const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(
     // URL
-    `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
+    `postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
 
     { logging: false, native: false }
 );
