1ef7b1ae48a7f0f1e1da6b060098c5f8730a4cc8
Daniel <daniel_alexis_12@hotmail.com>
2024-12-12 20:19:21 -0300

    ...

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  envDir: './env'
+  server: {
+    host: true, // Permite conexiones externas
+    port: 4173, // Asegúrate de usar el puerto que espera Railway
+  },
 })
