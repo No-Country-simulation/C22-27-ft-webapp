e9c8daef69ccfb5e3088fc9b23087704a7ce5a2a
Daniel <daniel_alexis_12@hotmail.com>
2024-12-12 19:55:55 -0300

    Delete Server

--- server/src/routes/session.routes.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require("express");
-const sessionControllers = require("../controllers/session.controller.js");
-
-const router = Router();
-
-router.post("/register", sessionControllers.register);
-router.post("/login", sessionControllers.login);
-
-module.exports = router;
